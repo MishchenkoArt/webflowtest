932167c7b30e4ac95b2c59167992cced00cfd9b0
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 23:27:05 +0300

    1

--- api/test.js
+++ api/test.js
@@ -6,7 +6,7 @@ const COLLECTION_ID = '662668afb17cfdeddcaffbee';
 const handler = async (req, res) => {
   console.log("hello"); // Вивести "hello" у консоль
   try {
-    const response = await axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
+    const response = await axios.get(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items`, {
       headers: {
         'Authorization': `Bearer ${API_KEY}`
       }
