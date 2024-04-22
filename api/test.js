823347b7400979347e95179887453ed64c2f0d52
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 21:11:08 +0300

    1

--- api/test.js
+++ api/test.js
@@ -1,23 +1,17 @@
-
-
 module.exports.handler = async (req, res) => {
   const axios = require('axios').default;
 
   const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
   const COLLECTION_ID = '662668afb17cfdeddcaffbee';
   
-  async function fetchData() {
-    try {
-      const response = await axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
-        headers: {
-          'Authorization': `Bearer ${API_KEY}`
-        }
-      });
-      console.log(response.data); // Дані з відповіді
-    } catch (error) {
-      console.error('Помилка:', error.response.status); // Статус помилки
-    }
+  try {
+    const response = await axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`
+      }
+    });
+    res.status(200).json(response.data); // Повернення даних як відповідь
+  } catch (error) {
+    res.status(error.response.status).json({ error: error.message }); // Повернення помилки як відповідь
   }
-  
-  fetchData();
-  };
+};
