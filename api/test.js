abc04595739b7364ffafc8a6b8741d02e6b103c2
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-23 01:12:07 +0300

    1

--- api/test.js
+++ api/test.js
@@ -16,12 +16,8 @@ const handler = async (req, res) => {
     console.log('Received email:', email);
 
     // Виконання POST запиту на test.com з використанням axios та тілом запиту
-    await axios.post('https://api.webflow.com/v2/collections/${COLLECTION_ID}/items', 
-    { "Email": email, 
-    fieldData: {
-      "name": "dsvsdv"
-    } 
-    }, 
+    await axios.get('https://api.webflow.com/v2/collections/${COLLECTION_ID}/items', 
+
     {
       headers: {
         'Authorization': `Bearer ${API_KEY}`,
