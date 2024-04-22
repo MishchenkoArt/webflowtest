c22cc18fb7ccf28b0700cce944726d23af426a3e
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 20:49:05 +0300

    1

--- api/main.js
+++ api/main.js
@@ -1,13 +1,26 @@
-// const axios = require('axios');
+const axios = require('axios').default;
 
-// const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
-// const COLLECTION_ID = '662668afb17cfdeddcaffbee';
+const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
+const COLLECTION_ID = '662668afb17cfdeddcaffbee';
 
-// axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
-//   headers: {
-//     'Authorization': `Bearer ${API_KEY}`
-//   }
-// })
+axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`
+  }
+})
+.then(function (response) {
+    // обработка успешного запроса
+    console.log(response);
+  })
+  .catch(function (error) {
+    // обработка ошибки
+    console.log(error);
+  })
+  .finally(function () {
+    // выполняется всегда
+  });
+
+
 // .then(response => {
 //   console.log(response.data); // Обробіть дані відповіді відповідним чином
 // })
@@ -17,7 +30,16 @@
 
 
 
-const app = require('axios')();
-app.get("/",(req, res)=>{
-    res.send("hello from express")
-})
+// const axios = require('axios').default;
+// axios.get('https://api.webflow.com/collections/${COLLECTION_ID}/items')
+//   .then(function (response) {
+//     // обработка успешного запроса
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // обработка ошибки
+//     console.log(error);
+//   })
+//   .finally(function () {
+//     // выполняется всегда
+//   });
