70ee7b4ce6642aa7eae5b22a09f1d4e2a660eadf
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 23:13:28 +0300

    1

--- api/test.js
+++ api/test.js
@@ -5,14 +5,6 @@ const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c99977
 const COLLECTION_ID = '662668afb17cfdeddcaffbee';
 
 module.exports.handler = async (req, res) => {
-  try {
-    const response = await axios.get(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items`, {
-      headers: {
-        'Authorization': `Bearer ${API_KEY}`
-      }
-    });
-    res.status(200).json(response.data); // Повернення даних як відповідь
-  } catch (error) {
-    res.status(error.response.status).json({ error: error.message }); // Повернення помилки як відповідь
-  }
+  console.log("hello"); // Вивести "hello" у консоль
+  res.status(200).json({ message: "hello" }); // Повернення "hello" як відповідь
 };
