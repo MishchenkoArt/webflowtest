e94b02eb8315a20aa0acc706aee6b6cffb5392d6
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 23:22:33 +0300

    1

--- api/test.js
+++ api/test.js
@@ -1,10 +1,11 @@
-
 const axios = require('axios').default;
 
 const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
 const COLLECTION_ID = '662668afb17cfdeddcaffbee';
 
-module.exports.handler = async (req, res) => {
+const handler = async (req, res) => {
   console.log("hello"); // Вивести "hello" у консоль
   res.status(200).json({ message: "hello" }); // Повернення "hello" як відповідь
 };
+
+module.exports = handler;
