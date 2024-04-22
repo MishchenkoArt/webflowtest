5bc0e2e31fa8932d272a821fc2cb57fcd6f59931
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 21:13:28 +0300

    1

--- api/test.js
+++ api/test.js
@@ -1,9 +1,10 @@
-module.exports.handler = async (req, res) => {
-  const axios = require('axios').default;
 
-  const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
-  const COLLECTION_ID = '662668afb17cfdeddcaffbee';
-  
+const axios = require('axios').default;
+
+const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
+const COLLECTION_ID = '662668afb17cfdeddcaffbee';
+
+module.exports.handler = async (req, res) => {
   try {
     const response = await axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
       headers: {
