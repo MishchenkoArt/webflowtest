a795e1bf3c952870b04d02b9be67b080b4eed892
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-23 00:55:58 +0300

    1

--- api/test.js
+++ api/test.js
@@ -7,7 +7,6 @@ const handler = async (req, res) => {
   try {
     // Отримання даних з тіла запиту, які містяться в полі email
     const { email } = req.body;
-    console.log('Received email:', email);
     // Перевірка чи є електронна пошта у тілі запиту
     if (!email) {
       return res.status(400).json({ error: 'Email is required' });
@@ -16,7 +15,15 @@ const handler = async (req, res) => {
     // Вивід електронної пошти в консоль
     console.log('Received email:', email);
 
-    // Повернення отриманої електронної пошти як відповідь
+    // Виконання POST запиту на test.com з використанням axios та тілом запиту
+    await axios.post('https://api.webflow.com/v2/collections/${COLLECTION_ID}/items', { Email: email }, {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // Відправлення відповіді клієнту
     res.status(200).json({ email });
   } catch (error) {
     // Відправлення помилки як відповідь у разі виникнення помилки
