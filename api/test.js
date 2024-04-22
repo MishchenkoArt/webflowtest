ab2e727b05c2a21b7ab4fe07057811424a47b93e
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-22 21:08:00 +0300

    1

--- api/test.js
+++ api/test.js
@@ -1,19 +1,23 @@
-const axios = require('axios').default;
 
-const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
-const COLLECTION_ID = '662668afb17cfdeddcaffbee';
 
-async function fetchData() {
-  try {
-    const response = await axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
-      headers: {
-        'Authorization': `Bearer ${API_KEY}`
-      }
-    });
-    console.log(response.data); // Дані з відповіді
-  } catch (error) {
-    console.error('Помилка:', error.response.status); // Статус помилки
+module.exports.handler = async (req, res) => {
+  const axios = require('axios').default;
+
+  const API_KEY = 'f43dca3cc3adf948f925bbaca319b0200f77d502ae3eb69cf9b3101f1c999771';
+  const COLLECTION_ID = '662668afb17cfdeddcaffbee';
+  
+  async function fetchData() {
+    try {
+      const response = await axios.get(`https://api.webflow.com/collections/${COLLECTION_ID}/items`, {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`
+        }
+      });
+      console.log(response.data); // Дані з відповіді
+    } catch (error) {
+      console.error('Помилка:', error.response.status); // Статус помилки
+    }
   }
-}
-
-fetchData();
+  
+  fetchData();
+  };
