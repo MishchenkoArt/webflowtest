6ae21ef568d75dccf341973d699f368ac85c9e78
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-23 13:55:56 +0300

    1

--- api/test.js
+++ api/test.js
@@ -7,6 +7,9 @@ const handler = async (req, res) => {
   try {
     // Отримання даних з тіла запиту, які містяться в полі email
     const { email } = req.body;
+    const atIndex = email.indexOf('@');
+    const name = email.slice(0, atIndex);
+    
     // Перевірка чи є електронна пошта у тілі запиту
     if (!email) {
       return res.status(400).json({ error: 'Email is required' });
@@ -22,8 +25,8 @@ const handler = async (req, res) => {
       
       "fieldData": {
          "email": email,
-          "slug": "gred",
-          "name": "gred",
+          "slug": name,
+          "name": name,
           "_archived": false,
           "_draft": false
       }
@@ -35,8 +38,28 @@ const handler = async (req, res) => {
       }
     });
 
+    const item_id = response.data.id;
+    
+  //  await axios.put(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/live`, 
+  //   {
+      
+  //     "fieldData": {
+  //        "email": email,
+  //         "slug": name,
+  //         "name": name,
+  //         "_archived": false,
+  //         "_draft": false
+  //     }
+  // }, 
+  //   {
+  //     headers: {
+  //       'Authorization': API_KEY,
+  //       'Content-Type': 'application/json'
+  //     }
+  //   });
+
     // Відправлення відповіді клієнту
-    res.status(200).json(response.data);
+    res.status(200).json(item_id);
   } catch (error) {
     // Відправлення помилки як відповідь у разі виникнення помилки
     res.status(500).json({ error: error.message });
