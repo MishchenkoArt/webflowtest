c6c7884a67596c739a6db36e4945d73320bdf471
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-23 14:31:44 +0300

    1

--- api/test.js
+++ api/test.js
@@ -5,59 +5,66 @@ const COLLECTION_ID = process.env.COLL_ID;
 
 const handler = async (req, res) => {
   try {
-    // Отримання даних з тіла запиту, які містяться в полі email
     const { email } = req.body;
     const atIndex = email.indexOf('@');
     const name = email.slice(0, atIndex);
     
-    // Перевірка чи є електронна пошта у тілі запиту
     if (!email) {
       return res.status(400).json({ error: 'Email is required' });
     }
 
-    // Вивід електронної пошти в консоль
     console.log('Received email:', email);
-    console.log (COLLECTION_ID, + " "+ API_KEY)
+    console.log(COLLECTION_ID, API_KEY);
 
-    // Виконання POST запиту на test.com з використанням axios та тілом запиту
+    // Перевірка чи існує вже електронна адреса в колекції Webflow
+    const existingEmailResponse = await axios.get(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items`, {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
+
+    // Перевірка, чи електронна адреса вже існує в колекції
+    const existingEmail = existingEmailResponse.data.items.some(item => item.fieldData.email === email);
+    if (existingEmail) {
+      return res.status(400).json({ error: 'Email already exists' });
+    }
+
+    // Якщо електронної адреси ще не існує, виконати POST запит для додавання нового запису
     const response = await axios.post(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/live`, 
-    {
-      
-      "fieldData": {
-         "email": email,
+      {
+        "fieldData": {
+          "email": email,
           "slug": name,
           "name": name,
           "_archived": false,
           "_draft": false
-      }
-  }, 
-    {
-      headers: {
-        'Authorization': API_KEY,
-        'Content-Type': 'application/json'
-      }
-    });
+        }
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      });
 
     const item_id = response.data.id;
     
-   await axios.patch(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/${item_id}/live`, 
-    {
-      
-      "fieldData": {
-         "id-field": item_id,
-      }
-  }, 
-    {
-      headers: {
-        'Authorization': API_KEY,
-        'Content-Type': 'application/json'
-      }
-    });
+    await axios.patch(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/${item_id}/live`, 
+      {
+        "fieldData": {
+          "id-field": item_id,
+        }
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      });
 
-    // Відправлення відповіді клієнту
     res.status(200).json(item_id);
   } catch (error) {
-    // Відправлення помилки як відповідь у разі виникнення помилки
     res.status(500).json({ error: error.message });
   }
 };
