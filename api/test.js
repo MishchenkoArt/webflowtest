893b6e622564b5f0e864d2f703208d7a291f0ca0
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-23 11:06:16 +0300

    1

--- api/test.js
+++ api/test.js
@@ -31,7 +31,7 @@ const handler = async (req, res) => {
     });
 
     // Відправлення відповіді клієнту
-    res.status(200).json(response);
+    res.status(200).json(response.data);
   } catch (error) {
     // Відправлення помилки як відповідь у разі виникнення помилки
     res.status(500).json({ error: error.message });
