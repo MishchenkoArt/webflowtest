d49fb9d8de10100d674ea686c6b86d6386899f01
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-24 11:28:50 +0300

    1

--- api/test.js
+++ api/test.js
@@ -1,25 +1,9 @@
-const express = require('express');
 const axios = require('axios').default;
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.json());
 
 const API_KEY = process.env.API_KEY;
 const COLLECTION_ID = process.env.COLL_ID;
-const ZDOROVISTOSUNKY_URL = 'https://www.zdorovistosunky.org';
 
-// Додамо CORS заголовки
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Дозволимо запити з будь-якого домену
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // Дозволимо певні методи запитів
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Дозволимо певні заголовки запитів
-  next();
-});
-
-// Роут для проксі запитів на сервер zdorovistosunky.org
-app.post('/api/proxy-to-zdorovistosunky', async (req, res) => {
+const handler = async (req, res) => {
   try {
     const { email } = req.body;
     const atIndex = email.indexOf('@');
@@ -29,18 +13,59 @@ app.post('/api/proxy-to-zdorovistosunky', async (req, res) => {
       return res.status(400).json({ error: 'Email is required' });
     }
 
-    // Виконання запиту на сервер zdorovistosunky.org
-    const response = await axios.post(`${ZDOROVISTOSUNKY_URL}/users`, { email });
+      // Перевірка чи існує вже електронна адреса в колекції Webflow
+    const existingEmailResponse = await axios.get(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items`, {
+      headers: {
+        'Authorization': `Bearer ${API_KEY}`,
+        'Content-Type': 'application/json'
+      }
+    });
 
-    res.status(200).json(response.data);
+    // Перевірка, чи електронна адреса вже існує в колекції
+    const existingEmail = existingEmailResponse.data.items.some(item => item.fieldData.email === email);
+    console.log(existingEmail)
+    if (existingEmail) {
+      console.log("yes")
+      return res.redirect(`https://www.zdorovistosunky.org/users/tema213132`);
+    }
+
+    // Якщо електронної адреси ще не існує, виконати POST запит для додавання нового запису
+    const response = await axios.post(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/live`, 
+      {
+        "fieldData": {
+          "email": email,
+          "slug": name,
+          "name": name,
+          "_archived": false,
+          "_draft": false
+        }
+      },
+      {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+    const item_id = response.data.id;
+    
+    await axios.patch(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/${item_id}/live`, 
+      {
+        "fieldData": {
+          "id-field": item_id,
+        }
+      }, 
+      {
+        headers: {
+          'Authorization': `Bearer ${API_KEY}`,
+          'Content-Type': 'application/json'
+        }
+      });
+
+    res.status(200).json(item_id);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-});
+};
 
-
-
-// Прослуховуємо порт 3000
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+module.exports = handler;
