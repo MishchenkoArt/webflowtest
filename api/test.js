254153d2cdd82302aa8944835a15c73984fcc12f
MishchenkoArt <58958235+MishchenkoArt@users.noreply.github.com>
2024-04-24 18:04:32 +0300

    1

--- api/test.js
+++ api/test.js
@@ -20,49 +20,51 @@ const handler = async (req, res) => {
         'Authorization': `Bearer ${API_KEY}`,
         'Content-Type': 'application/json'
       }
+
     });
 
     // Перевірка, чи електронна адреса вже існує в колекції
     const existingEmail = existingEmailResponse.data.items.some(item => item.fieldData.email === email);
-    console.log(existingEmail)
-    if (!existingEmail) {
+    if (existingEmail){
+      res.status(200).json(existingEmail.fieldData.slug);
+    } else{
+
       console.log("no")
-          // Якщо електронної адреси ще не існує, виконати POST запит для додавання нового запису
-    const response = await axios.post(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/live`, 
-    {
-      "fieldData": {
-        "email": email,
-        "slug": name,
-        "name": name,
-        "_archived": false,
-        "_draft": false
-      }
-    },
-    {
-      headers: {
-        'Authorization': `Bearer ${API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    });
+      // Якщо електронної адреси ще не існує, виконати POST запит для додавання нового запису
+const response = await axios.post(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/live`, 
+{
+  "fieldData": {
+    "email": email,
+    "slug": "",
+    "name": name,
+    "_archived": false,
+    "_draft": false
+  }
+},
+{
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`,
+    'Content-Type': 'application/json'
+  }
+});
 
-  const item_id = response.data.id;
-  
-  await axios.patch(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/${item_id}/live`, 
-    {
-      "fieldData": {
-        "id-field": item_id,
-      }
-    }, 
-    {
-      headers: {
-        'Authorization': `Bearer ${API_KEY}`,
-        'Content-Type': 'application/json'
-      }
-    });
+const item_id = response.data.id;
 
-  res.status(200).json(slug);
+await axios.patch(`https://api.webflow.com/v2/collections/${COLLECTION_ID}/items/${item_id}/live`, 
+{
+  "fieldData": {
+    "id-field": item_id,
+  }
+}, 
+{
+  headers: {
+    'Authorization': `Bearer ${API_KEY}`,
+    'Content-Type': 'application/json'
+  }
+});
+
+res.status(200).json(slug);
     }
-
 
   } catch (error) {
     res.status(500).json({ error: error.message });
